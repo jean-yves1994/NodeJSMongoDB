c4e5e2fb9267cc34fdaaa5a83c1b217808a05372
jean-yves1994 <roida@KINGBEATS-PC>
2019-09-23 22:53:40 +0200

    to the github

---
+++ index.js
@@ -0,0 +1,134 @@
+var mongodb = require('mongodb');
+var ObjectID = mongodb.ObjectID;
+var crypto = require('crypto');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+//PASSWORD UTILS
+//CREATE FUNCTION TO RANDOM SALT
+var genRandomString = function(length){
+    return crypto.randomBytes(Math.ceil(length/2))
+    .toString('hex')
+    .slice(0,length);
+};
+
+var sha512 = function(password,salt){
+    var hash = crypto.createHmac('sha512',salt);
+    hash.update(password);
+    var value = hash.digest('hex');
+    return{
+        salt:salt,
+        passwordHash:value
+    };
+};
+
+function saltHashPassword(userPassword){
+    var salt = genRandomString(16); //Create 16 random characters
+    var passwordData = sha512(userPassword,salt);
+    return passwordData;
+}
+
+function checkHashPassword(userPassword,salt){
+    var passwordData = sha512(userPassword,salt);
+    return passwordData;
+}
+
+//Create Express Services
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Create MongoDB Client
+var MongoClient = mongodb.MongoClient;
+
+//Coonection URL
+var url = 'mongodb://localhost:27017' //Defaul port 
+
+MongoClient.connect(url,{useNewUrlParser: true},function(err,client){
+    if(err)
+    console.log('Unable to connect to mongoDB server', err);
+    else{
+
+        //Register
+        app.post('/register',(request,response,next)=>{
+            var post_data = request.body;
+            
+            var plaint_password = post_data.password;
+            var hash_data = saltHashPassword(plaint_password);
+
+            var password = hash_data.passwordHash; //Save password hash
+            var salt = hash_data.salt; //Save salt
+
+            var name = post_data.name;
+            var email = post_data.email;
+            var phone = post_data.phone;
+
+            var insertJson={
+                'email':email,
+                'password':password,
+                'salt':salt,
+                'phone': phone,
+                'name': name
+            };
+            var db = client.db('secureid');
+
+            //Check existed email
+            db.collection('user')
+            .find({'email':email}).count(function(err,number){
+                if(number !=0)
+                {
+                    response.json('Email already exists');
+                    console.log('Email already exists');
+                }
+                else{
+                    db.collection('user')
+                    .insertOne(insertJson,function(error,res){
+                        response.json('Registration success');
+                    console.log('Registration success');
+                    })
+                }
+            })
+        });
+
+        app.post('/login',(request,response,next)=>{
+            var post_data = request.body;
+            
+            var email = post_data.email;
+            var userPassword = post_data.password;
+
+            var db = client.db('secureid');
+
+            //Check existed email
+            db.collection('user')
+            .find({'email':email}).count(function(err,number){
+                if(number ==0)
+                {
+                    response.json('Email does not exist');
+                    console.log('Email does not exist');
+                }
+                else{
+                    db.collection('user')
+                    .findOne({'email':email},function(err,user){
+                        var salt = user.salt; //Get salt from user
+                        var hashed_password = checkHashPassword(userPassword,salt).passwordHash;
+                        var encrypted_password = user.password;
+                        if(hashed_password ==encrypted_password)
+                        {
+                            response.json('Login success');
+                            console.log('Login success');
+                        }
+                        else{
+                            response.json('Wrong password');
+                            console.log('Wrong password'); 
+                        }
+                    })
+                }
+            })
+        });
+        //Start web server
+        app.listen(3000,()=>{
+            console.log('Connected to MongoDB server, WebService running on port 3000');
+        })
+    }
+});
